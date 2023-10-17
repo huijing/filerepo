fe2b2d9cdaaf190134d0d8bfcf163da73be82149
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-10-17 13:53:38 +0800

    Update incoming-grant.ts

--- incoming-grant.ts
+++ incoming-grant.ts
@@ -1,4 +1,4 @@
-// Import dependencies
+//@! start chunk 1 | title=Import dependencies
 import dotenv from "dotenv";
 import { join } from "path";
 import { createAuthenticatedClient, isPendingGrant } from "@interledger/open-payments";
@@ -8,21 +8,22 @@ dotenv.config({ path: join(__dirname, "..", ".env") });
 
 const KEY_ID = process.env.KEY_ID;
 const PAYMENT_POINTER = process.env.PAYMENT_POINTER;
+//@! end chunk 1
 
 async function run() {
-    // Initialize Open Payments client
+//@! start chunk 2 | title=Initialize Open Payments client
     const client = await createAuthenticatedClient({
         paymentPointerUrl: PAYMENT_POINTER,
         privateKey: loadPrivateKey(),
         keyId: KEY_ID,
     });
-
-    // Get payment pointer information
+//@! end chunk 2
+//@! start chunk 3 | title=Get payment pointer information
     const paymentPointer = await client.paymentPointer.get({
         url: PAYMENT_POINTER,
     });
-
-    // Request incoming payment grant
+//@! end chunk 3
+//@! start chunk 4 | title=Request incoming payment grant
     const grant = await client.grant.request(
         {
             url: paymentPointer.authServer,
@@ -46,5 +47,5 @@ async function run() {
     console.log("INCOMING_PAYMENT_ACCESS_TOKEN =", grant.access_token.value);
     console.log("INCOMING_PAYMENT_ACCESS_TOKEN_MANAGE_URL = ", grant.access_token.manage);
 }
-
+//@! end chunk 4
 run();
